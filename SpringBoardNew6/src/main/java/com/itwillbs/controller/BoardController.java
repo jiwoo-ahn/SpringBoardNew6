358c978cab56e7a92a437baac4dcc57dd8421007
“jiwoo-ahn” <dkswlw00@gmail.com>
2025-04-09 17:49:12 +0900

    게시판 글 정보 입력(view페이지) 생성 완료 + 템플릿 적용 완료

--- SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.itwillbs.service.BoardService;
 
 @Controller
+@RequestMapping(value = "/board/*")
 public class BoardController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
@@ -20,6 +21,7 @@ public class BoardController {
 	@Inject
 	private BoardService bService;
 	
+	// http://localhost:8088/board/time
 	@RequestMapping(value = "/time", method = RequestMethod.GET)
 	public void getServerTimeGET(Model model) {
 		logger.info(" DB 서버의 시간 정보를 가져오기");
@@ -29,4 +31,14 @@ public class BoardController {
 		model.addAttribute("time", time);
 		logger.info("연결된 뷰페이지에 정보 전달");
 	}
+	
+	// 게시판 글쓰기 - GET
+	// http://localhost:8088/board/regist
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public void registGET() throws Exception{
+		logger.info("registGET() 실행");
+		logger.info("글쓰기 뷰페이지(/board/regist.jps)를 연결해서 보여줌");
+	}
+	
+	// 게시판 글쓰기 - POST
 }
